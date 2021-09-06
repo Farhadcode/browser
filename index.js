763184fbf30eef817fd1b466f4d77554414751b2
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-06 14:38:53 +0600

    home work done

---
+++ index.js
@@ -0,0 +1,35 @@
+/// 1st problem done
+/*
+console.log('farhad');
+console.log('love');
+setTimeout(() => {
+    console.log('hello baby I am coming after  3.5  scecond');
+}, 3500);
+
+console.log('ato khon kothay chila tumi');
+
+// 2nd
+const sum = () => {
+    let value = prompt('Give a nubmer value');
+    const result = parseInt(value) + 5;
+    const alertResult = alert(result);
+
+    // console.log(result);
+}
+
+//sum();
+
+ // 3rd worke done
+
+const seeSomthing = () => {
+    const value = confirm('if you want to see the href');
+    //console.log(value);
+    if (value === true) {
+        console.log('href k dakhidao');
+    }
+
+}
+seeSomthing();
+
+*/
+

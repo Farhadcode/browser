4908fa3fa581da04b0ce9eb8958fcada0c03e229
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-06 12:43:58 +0600

    add shopping cad

--- shopping.js
+++ shopping.js
@@ -1,8 +1,19 @@
 
+const displayLocalStorageCart = () => {
+    const cart = getCard();
+    for (const name in cart) {
+        displayProduct(name);
+
+    }
+}
+
+
 const addItems = () => {
     const productNameField = document.getElementById('produt-name');
     const name = productNameField.value;
-
+    if (!name) {
+        return;
+    }
     //display in the UI 
 
     displayProduct(name);
@@ -34,7 +45,19 @@ const getCard = () => {
 
 const addProductToCart = name => {
     const cart = getCard();
-    cart[name] = 1;
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+    else {
+        cart[name] = 1;
+    }
+
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified);
 }
+
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
+}
+displayLocalStorageCart();
